74f888816a4d634a182165bd183cedd08ecaf887
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-09-30 14:27:12 -0400

    updated profile controller for Postgres key words

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -3,17 +3,26 @@ const db = require("../models")
 
 const { Book, User, UserFavorite } = db
 
-// INDEX
-router.get('/', async (req, res) => {
-    const findUser = await db.User.find()
-    try{
-      res.status(200).json(findUser)
+// SHOW
+router.get('/:profileId', async (req, res) => {
+    let profileId = Number(req.params.profileId)
+    if (isNaN(profileId)) {
+        res.status(404).json({ message: `Invalid id "${profileId}"` })
+    } else {
+        const profile = await Profile.findOne({
+            where: { profileId: profileId },
+            // include: {
+            //     association: 'userFavorite',
+            //     include: 'user'
+            // }
+        })
+        if (!profile) {
+            res.status(404).json({ message: `Could not find place with id "${profileId}"` })
+        } else {
+            res.json(profile)
+        }
     }
-    catch(err){
-      console.log('err', err);
-      res.status(500).json(err);
-    }
-  })
+})
 
 //   userFavorite controller
 
