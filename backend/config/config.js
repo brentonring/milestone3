12e7c798eaebef80d7e5a6226c8859884359cdf7
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-10-01 19:30:21 -0400

    updated config.js file with db info

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,9 +16,9 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
