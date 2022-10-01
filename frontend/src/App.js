0b674789374f582bf8ab84a12d2976a907e0aa90
Brenton Ring <97139815+brentonring@users.noreply.github.com>
2022-10-01 10:04:02 -0600

    resolved conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
 import RelitAppBar from './components/AppBar.js';
+import {getBooks} from './api/axios'
 import React from 'react';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [books, setBooks] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    getBooks().then(json => {
+      setBooks(json)
+      return json
+    }).then(json => {
+      setSearchResults(json)
+    })
+  }, [])
   return (
     <div className="App">
       <RelitAppBar/>
