d8b918d3e892ff6d4a70c9930ef828ad775f4ca4
Katie Eells <katieeells@Katies-MBP.lan>
2022-10-01 09:24:33 -0400

    Footer and HomeBody added, needs further edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import RelitAppBar from './components/AppBar.js';
+import Footer from './components/Footer.js';
+import HomeBody from './components/HomeBody.js';
 
 function App() {
   return (
     <div className="App">
       <RelitAppBar/>
-      <h1>Hello World</h1>     
+      <HomeBody/>
+      <Footer/>
     </div>
   );
 }
--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -9,11 +9,11 @@ function RelitAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar sx={{bgcolor: '#0B295B'}} position="static">
-            <Toolbar  style={{ height: '100px' }} variant="dense">
-                <Typography sx={{ fontWeight: 400, flexGrow: 1 }} variant="h2" color='#EC412F' component="div" >
-                Relit Books
+            <Toolbar style={{ height: '100px' }} variant="dense">
+                <Typography sx={{ fontWeight: 400, flexGrow: 1, ml: 22 }} variant="h2" color='#EC412F' component="div" >
+                    Relit Books
                 </Typography>
-                <Button style={{ fontSize: 16, color: '#F7F7F7' }} sx={{ mr: 1 }}>Sign Up</Button>
+                <Button style={{ fontSize: 16, color: '#F7F7F7' }} sx={{ mr: 2 }}>Sign Up</Button>
                 <Button style={{ fontSize: 16, color: '#F7F7F7' }} sx={{ mr: 1 }}>Login</Button>
             </Toolbar>
             </AppBar>
