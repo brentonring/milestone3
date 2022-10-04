f4b08bef81f4e73d23a70561789b81215f27d56b
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-10-03 20:44:34 -0400

    working on backend controllers

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,22 +1,22 @@
-const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
+// const router = require('express').Router()
+// const db = require("../models")
+// const bcrypt = require('bcrypt')
 
-const { User } = db
+// const { User } = db
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
     
-    let user = await User.findOne({
-        where: { email: req.body.email }
-    })
+//     let user = await User.findOne({
+//         where: { email: req.body.email }
+//     })
 
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
-        })
-    } else {
-        res.json({ user })
-    }
-})
+//     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+//         res.status(404).json({ 
+//             message: `Could not find a user with the provided username and password` 
+//         })
+//     } else {
+//         res.json({ user })
+//     }
+// })
 
-module.exports = router
+// module.exports = router
