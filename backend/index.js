599257698698694b992bea74c855b9b01ad29f69
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-10-07 19:05:08 -0400

    modified index.js to render all static files

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,12 @@ app.use("/users", require("./controllers/users"));
 
 // serve static front end in production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, ".", "client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "client/build/index.html"));
+});
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
